3c2e65b5ad8861497f39e624596a41658251da30
WilianBarahona <wilianbarahona10m@gmail.com>
2019-09-06 18:16:40 -0600

    express-handlebars

--- rest-api/app.js
+++ rest-api/app.js
@@ -1,6 +1,7 @@
 'use strict'
 const express = require('express') 
 const bodyParser = require('body-parser')
+const hbs = require('express-handlebars')
 const app = express()
 
 const productRouter = require('./routes/product-route')
@@ -9,5 +10,20 @@ const productRouter = require('./routes/product-route')
 app.use(bodyParser.urlencoded({extended: false}))                         
 app.use(bodyParser.json())
 app.use('/api', productRouter)
+
+//Configurar el motor de plantillas de hbs
+app.engine('.hbs', hbs({ // el motor recibe archivos .hbs 
+    defaultLayout: 'default', // default.hbs => donde van el doctype, js, css y esqueleto principal de un html
+    extname: '.hbs' //la extencion por defecto es .handlebars
+}))
+
+//express-handlebars busca por defecto una carpeta llamada views, dentro de esta carpte
+// busca un archivo padre donde contiene el esqueleto del html con los css y js necesarios
+
+app.set('view engine' , '.hbs') //usar motor de vistas con extencion .hbs
+
+app.get('/login', (req, res)=>{
+    res.render('login')
+})
 
 module.exports = app
